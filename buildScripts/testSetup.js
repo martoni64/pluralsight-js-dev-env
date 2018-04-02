6ab859d59384337eeb1505daf5baa77b7375b3fc
unknown <martin@skjoldebrand.org>
2018-04-02 07:20:27 +0200

    Final updates

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,6 +1,6 @@
-// Not transpiled so must use CommonJs and ES5
+// This file isn't transpiled, so must use CommonJS and ES5
 
-// Register babel to transpile before our tests are run.
+// Register babel to transpile before our tests run.
 require('babel-register')();
 
 // Disable webpack features that Mocha doesn't understand.
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,27 +1,30 @@
-import webpack from 'webpack';
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    debug: true,
-    devtool: 'inline-source-map',
-    noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
-    target: 'web',
-    output: {
-        path: path.resolve(__dirname, 'src'),
-        publicpath: '/',
-        filename: 'bundle.js'
-    },
-    /*devServer: {
-        contentBase: path.resolve(__dirname, 'src')
-    },*/
-    plugins: [],
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
-        ]
-    }
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
 }
